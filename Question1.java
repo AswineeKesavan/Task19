c7865dcf76a7ece7da633dccd0674672d9f4e830
aswineeKesavan <aswineek7@gmail.com>
2024-09-08 22:51:56 +0530

    second commit

--- Question1.java
+++ Question1.java
@@ -10,10 +10,9 @@ public class Question1 {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriver driver = new FirefoxDriver(); // intializing the fireFoxDriver
-		driver.navigate().to("http://google.com");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		Thread.sleep(3000);
+		driver.navigate().to("http://google.com");	
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		String url=driver.getCurrentUrl();
 		System.out.println("The current url of the page:"+url);
 		driver.navigate().refresh();
--- Question2.java
+++ Question2.java
@@ -9,9 +9,10 @@ public class Question2 {
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriver driver = new ChromeDriver(); // intializing the chromeDriver
-		driver.navigate().to("https://www.demoblaze.com");
 		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.navigate().to("https://www.demoblaze.com");
+		Thread.sleep(1000);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		String title=driver.getTitle();
 		if(title.equalsIgnoreCase("STORE")) {
 			System.out.println("Page landed on correct website:"+title);
